13a71a19da20e1b498889dd32867acf64a5aa2ef
Franco Nuñez <fnunez@novared.net>
2021-05-11 12:23:17 -0300

    texto a voz es-en

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+
+// document.querySelector("#Hablar").addEventListener("click",()=>{
+//     decir(document.getElementById('texto').value);
+// });
+
+
+//   function decir(texto) {
+//     if (speechSynthesis.speaking) {
+//         // speechSynthesis.cancel();
+//     }
+//     else {
+//         var message = new SpeechSynthesisUtterance(texto);
+//         message.lang = "de";
+//         speechSynthesis.speak(message);
+//     }
+// }
+
+
+// speechSynthesis.getVoices();
+ 
+// setTimeout(function () {
+//     // After 1 second, get the voices now Chrome is listening.
+//     speechSynthesis.getVoices().forEach(function (voice) {
+//       console.log('Hi! My name is ', voice.name);
+//     });
+// }, 1000);
+
+
+
+
+
+
