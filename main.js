89753bb5080d7f767f318439f870c156897de075
Franco Nuñez <fnunez@novared.net>
2021-05-12 00:22:49 -0300

    diseño en boostrap, falta fondo particles y funcionalidad js

--- main.js
+++ main.js
@@ -6,26 +6,26 @@
 // });
 
 
-//   function decir(texto) {
-//     if (speechSynthesis.speaking) {
-//         // speechSynthesis.cancel();
-//     }
-//     else {
-//         var message = new SpeechSynthesisUtterance(texto);
-//         message.lang = "de";
-//         speechSynthesis.speak(message);
-//     }
-// }
+  function decir(texto) {
+    if (speechSynthesis.speaking) {
+        // speechSynthesis.cancel();
+    }
+    else {
+        var message = new SpeechSynthesisUtterance(texto);
+        message.lang = "de";
+        speechSynthesis.speak(message);
+    }
+}
 
 
-// speechSynthesis.getVoices();
+speechSynthesis.getVoices();
  
-// setTimeout(function () {
-//     // After 1 second, get the voices now Chrome is listening.
-//     speechSynthesis.getVoices().forEach(function (voice) {
-//       console.log('Hi! My name is ', voice.name);
-//     });
-// }, 1000);
+setTimeout(function () {
+    // After 1 second, get the voices now Chrome is listening.
+    speechSynthesis.getVoices().forEach(function (voice) {
+      console.log('Hi! My name is ', voice.name);
+    });
+}, 1000);
 
 
 
